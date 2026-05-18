53727009bed2d86345518da489a66f48a0b5cb18
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-18 16:50:45 -0300

    Enable user-scoped storage before app mount

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { setupUserScopedLocalStorage } from "./auth.js";
+
+setupUserScopedLocalStorage();
+
 createRoot(document.getElementById("root")).render(<App />);
